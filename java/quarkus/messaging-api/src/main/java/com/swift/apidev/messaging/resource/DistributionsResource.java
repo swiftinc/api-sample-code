c3eb079fc36678e1377a08dad816f7949fcb92a2
Iker Mendilibar <reki03@gmail.com>
2024-07-04 12:46:09 +0200

    Messaging API 2.0

--- java/quarkus/messaging-api/src/main/java/com/swift/apidev/messaging/resource/DistributionsResource.java
+++ java/quarkus/messaging-api/src/main/java/com/swift/apidev/messaging/resource/DistributionsResource.java
@@ -4,10 +4,15 @@ import com.swift.apidev.messaging.oas.api.ApiException;
 import com.swift.apidev.messaging.oas.api.DistributionsApi;
 import com.swift.apidev.messaging.oas.model.*;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
 @Path("/distributions")
@@ -22,5 +27,17 @@ public class DistributionsResource {
     public Response listDistributions() throws ApiException {
         ListDistributionsResponse distributions = distributionsApi.listDistributions(100, 0);
         return Response.ok(distributions).build();
+    }
+
+    @POST
+    @Path("/{distributionId}/nak")
+    @Operation(summary = "Nack distribution")
+    public Response listDistributions(
+            @Parameter(required = true, example = "44984189500", schema = @Schema(type = SchemaType.STRING)) @PathParam("distributionId") Long distributionId)
+            throws ApiException {
+        Nak nak = new Nak();
+        nak.setReason("");
+        distributionsApi.nakDistribution(distributionId, nak);
+        return Response.noContent().build();
     }
 }
